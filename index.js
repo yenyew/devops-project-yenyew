8c0305bf3210599cd24cec475b8a8d0467da05d7
jiayuuu64 <2305136J@student.tp.edu.sg>
2024-11-04 16:04:26 +0800

    i added the database

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+// Importing mongoose module for MongoDB database operations
+const mongoose = require('mongoose');
+// Importing dotenv module to load environment variables from a .env file into process.env
+require('dotenv').config();
 var app = express();
 const PORT = process.env.PORT || 5050;
 var startPage = "index.html";
@@ -9,6 +13,17 @@ app.use(express.static("./public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/" + startPage);
 });
+
+// Setting mongoose to use strict query
+mongoose.set('strictQuery', true);
+
+console.log(process.env.DB_CONNECT)
+// Connecting to MongoDB database using the connection string from environment variables
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log('MongoDB connected. . .'); // Log when successfully connected
+    });
+
 server = app.listen(PORT, function () {
   const address = server.address();
   const baseUrl = `http://${
@@ -17,3 +32,4 @@ server = app.listen(PORT, function () {
   console.log(`Demo project at: ${baseUrl}`);
 });
 module.exports = { app, server };
+
