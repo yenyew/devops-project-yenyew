6a790074c1863db7a7c06a927656b0b7661863c2
Sakineee <kohbrayden@gmail.com>
2024-11-04 15:34:43 +0800

    included models and utils for jobs

---
+++ utils/JobUtil.js
@@ -0,0 +1,29 @@
+const { Resource } = require("../models/Resource");
+const fs = require("fs").promises;
+async function readJSON(filename) {
+  try {
+    const data = await fs.readFile(filename, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function writeJSON(object, filename) {
+  try {
+    const allObjects = await readJSON(filename);
+    allObjects.push(object);
+    await fs.writeFile(filename, JSON.stringify(allObjects), "utf8");
+    return allObjects;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+module.exports = {
+  readJSON,
+  writeJSON,
+  addResource,
+};
