f7036c2cbde5b916a19df1c00be46ffa119aca53
Sakineee <kohbrayden@gmail.com>
2024-11-09 15:06:36 +0800

    Updated Search Bar

--- utils/search-job.js
+++ utils/search-job.js
@@ -2,24 +2,20 @@ const Job = require('../models/jobs');
 
 async function searchJobs(req, res) {
     try {
-        const { keyword, location, classification } = req.query;
+        const { keyword, location } = req.query;
 
         // Build query object
         let query = {};
 
         if (keyword) {
             query.$or = [
-                { name: new RegExp(keyword, 'i') },
-                { description: new RegExp(keyword, 'i') }
+                { name: new RegExp(keyword, 'i') }, // Search by job name
+                { companyName: new RegExp(keyword, 'i') } // Search by company name
             ];
         }
 
         if (location && location !== 'Any Location') {
-            query.location = new RegExp(location, 'i');
-        }
-
-        if (classification && classification !== 'Any Classification') {
-            query.classification = new RegExp(classification, 'i');  
+            query.location = new RegExp(location, 'i'); // Search by location
         }
 
         // Find jobs based on query
